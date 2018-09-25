cdef6b0652b665b6ce02bacd683d8cdb0589ba12
Michal Szajnecki <michal.szajnecki@gmail.com>
2018-09-25 10:27:34 +0200

    Small cleanup and input for LShash

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var localData;
+var storageData;
 
 // Inject data
-chrome.tabs.query({url: 'http://orteil.dashnet.org/cookieclicker/' }, function(params) {
+chrome.tabs.query({
+  url: 'http://orteil.dashnet.org/cookieclicker/'
+}, function (params) {
   chrome.tabs.executeScript(params[0].id, {
     file: "js/injector.js"
   });
@@ -9,36 +12,35 @@ chrome.tabs.query({url: 'http://orteil.dashnet.org/cookieclicker/' }, function(p
 
 
 chrome.runtime.onMessage.addListener(function (msg) {
-console.log(msg);
-localData = msg;
-let localDiv = document.getElementById('localStorage');
-localDiv.innerHTML = msg.ascendMeter + ' | ' + msg.date;
-addBTNlistener();
+  console.log(msg);
+  localData = msg;
+  let localDiv = document.getElementById('localStorage');
+  localDiv.innerHTML = msg.ascendMeter + ' | ' + msg.date;
+  addBTNlistener();
 });
-// chrome.storage.sync.set({'CookieClickerGameSave': zed}, function() {
-//     console.log('Value is set to ' + zed);
-//   });
 
 function addBTNlistener(params) {
-  
+
   let btn = document.getElementById('saveBtn');
-  btn.addEventListener('click', function() {
-    chrome.storage.sync.set({'emCC': JSON.stringify(localData)}, function() {
+  btn.addEventListener('click', function () {
+    chrome.storage.sync.set({
+      'emCC': JSON.stringify(localData)
+    }, function () {
       console.log('Value is set to ' + JSON.stringify(localData));
     });
+  })
+
+  let copyBtn = document.getElementById('copyBtn');
+  copyBtn.addEventListener('click', function (event) {
+    let input = document.getElementById('chromeStorageHash');
+    input.value = storageData
   })
 }
 
 
-  chrome.storage.sync.get(['emCC'], function(result) {
-    console.log(result);
-    result = JSON.parse(result.emCC);
-    let chromeStorageDiv = document.getElementById('chromeStorage');
-    chromeStorageDiv.innerHTML = result.ascendMeter + ' | ' + result.date;
-  });
-
-  
-
-// chrome.storage.sync.getBytesInUse(['CookieClickerGameSave'], function(result) {
-//     console.log(result);
-//   });
+chrome.storage.sync.get(['emCC'], function (result) {
+  result = JSON.parse(result.emCC);
+  storageData = result.CookieClickerGame
+  let chromeStorageDiv = document.getElementById('chromeStorage');
+  chromeStorageDiv.innerHTML = result.ascendMeter + ' | ' + result.date;
+});
